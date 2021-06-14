7cc8f636a94c36c5c6b12763050c56023f03020d
dcheng <cheng.dorothy.s@gmail.com>
2021-06-14 16:26:39 -0600

    Updated comment create route

--- routes/commentsRoutes.js
+++ routes/commentsRoutes.js
@@ -3,7 +3,7 @@ const ctrl = require('../controllers');
 
 router.get('/', ctrl.comments.index);
 router.get('/:id', ctrl.comments.show);
-router.post('/:postId', ctrl.comments.create);
+router.post('/', ctrl.comments.create);
 router.put('/:id', ctrl.comments.update);
 router.delete('/:id', ctrl.comments.destroy);
 
